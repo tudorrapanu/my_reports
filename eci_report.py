9caa5828c404c4fd0888ebf80f1eb57244e4a580
tudorrapanu <51211750+tudorrapanu@users.noreply.github.com>
2020-06-08 18:42:30 +0300

    Update eci_report.py

--- eci_report.py
+++ eci_report.py
@@ -11,49 +11,49 @@ dbr_client,
 dbr_no,
 dbr_name1,
 dbr_cli_ref_no,
-(select isnull(sum(ivt_paid), 0)
-        from cds.ivt
+(select ifnull(sum(ivt_paid), 0)
+        from CDS.IVT
         where ivt_dbr_no = dbr_no) as amt_paid,
-(select isnull(sum(ivt_amount-ivt_paid), 0) 
-        from cds.ivt 
+(select ifnull(sum(ivt_amount-ivt_paid), 0) 
+        from CDS.IVT 
         where ivt_dbr_no = dbr_no 
         and inv_past_due_bucket=0) as bucket0, 
-(select isnull(sum(ivt_amount-ivt_paid), 0) 
-        from cds.ivt 
+(select ifnull(sum(ivt_amount-ivt_paid), 0) 
+        from CDS.IVT 
         where ivt_dbr_no = dbr_no 
         and inv_past_due_bucket=30) as bucket30,
-(select isnull(sum(ivt_amount-ivt_paid), 0)  
-        from cds.ivt 
+(select ifnull(sum(ivt_amount-ivt_paid), 0)  
+        from CDS.IVT 
         where ivt_dbr_no = dbr_no 
         and inv_past_due_bucket=60) as bucket60, 
-(select isnull(sum(ivt_amount-ivt_paid), 0)  
-        from cds.ivt 
+(select ifnull(sum(ivt_amount-ivt_paid), 0)  
+        from CDS.IVT 
         where ivt_dbr_no = dbr_no 
         and inv_past_due_bucket=90) as bucket90, 
-(select isnull(sum(ivt_amount-ivt_paid), 0) 
-        from cds.ivt 
+(select ifnull(sum(ivt_amount-ivt_paid), 0) 
+        from CDS.IVT
         where ivt_dbr_no = dbr_no 
         and inv_past_due_bucket=120) as bucket120,
-(select isnull(sum(ivt_amount-ivt_paid), 0) 
-        from cds.ivt 
+(select ifnull(sum(ivt_amount-ivt_paid), 0) 
+        from CDS.IVT 
         where ivt_dbr_no = dbr_no 
         and inv_past_due_bucket=180) as bucket180,
-(select isnull(sum(ivt_amount-ivt_paid), 0)  
-        from cds.ivt 
+(select ifnull(sum(ivt_amount-ivt_paid), 0)  
+        from CDS.IVT 
         where ivt_dbr_no = dbr_no 
         and inv_past_due_bucket=181) as bucket181,  
 dbr_status, 
 dbr_last_worked_o,
 (select max(dat_Seq_no) 
-        from cds.dat 
+        from CDS.DAT 
         where dat_dbr_no = dbr_no 
         and dat_note = 'Y') as max_seq,
-(select list(dnt_note, '' order by dnt_seq_sub) 
-        from cds.dnt 
+(select GROUP_CONCAT(DNT_NOTE ORDER BY DNT_SEQ_SUB SEPARATOR  '') 
+        from CDS.DNT 
         where dnt_dbr_no = dbr_no 
         and dnt_seq_no = max_seq) as last_note,
 dbr_desk
-from cds.dbr
+from CDS.DBR
 where dbr_client in ('ECI099', 'ECI098', 'ECI097')
 group by
 dbr_status, 
